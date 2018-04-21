ed6bedd89c0e033ba06b3af8461e0fd1ab2442f3
Dongfang Zhao <orientzh1230@gmail.com>
2018-04-20 19:32:00 -0700

    Complete Pagination for Results TableView

--- TandESearchIOS/ResultsTableViewController.swift
+++ TandESearchIOS/ResultsTableViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
+import AlamofireSwiftyJSON
 
 class ResultsTableViewController: UITableViewController {
 
-    var placeResult = [JSON]()
+    @IBOutlet weak var previousButton: UIBarButtonItem!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    @IBOutlet var resultsTableView: UITableView!
+    let serverUrlPrefix = "http://localhost:3000/"
+    var placeResults = [[JSON]]()
+    var placeResultDisplay = [JSON]()
+    var page = 0
+    var nextPageToken = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +30,31 @@ class ResultsTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        previousButton.isEnabled = false
+        checkNextButtonStatus()
     }
     
     func setTable(resultsObject: JSON) {
-        placeResult = resultsObject["results"].arrayValue
+        page = 0
+        placeResults.removeAll()
+        placeResultDisplay = resultsObject["results"].arrayValue
+        placeResults.append(placeResultDisplay)
+        if resultsObject["next_page_token"].string != nil {
+            nextPageToken = resultsObject["next_page_token"].string!
+        }
+        if isViewLoaded {
+            checkNextButtonStatus()
+        }
+    }
+    
+    func checkNextButtonStatus() {
+        print(nextPageToken)
+        if nextPageToken.isEmpty {
+            nextButton.isEnabled = false
+        }
+        else {
+            nextButton.isEnabled = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,7 +66,7 @@ class ResultsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        if (!placeResult.isEmpty) {
+        if (!placeResultDisplay.isEmpty) {
             return 1
         }
         else {
@@ -52,18 +82,18 @@ class ResultsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return placeResult.count
+        return placeResultDisplay.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "place", for: indexPath)
 
-        cell.textLabel?.text = placeResult[indexPath.row]["name"].string
+        cell.textLabel?.text = placeResultDisplay[indexPath.row]["name"].string
         cell.textLabel?.numberOfLines = 0
-        cell.detailTextLabel?.text = placeResult[indexPath.row]["vicinity"].string
+        cell.detailTextLabel?.text = placeResultDisplay[indexPath.row]["vicinity"].string
         cell.detailTextLabel?.numberOfLines = 0
-        let icon = try! Data(contentsOf: URL(string: placeResult[indexPath.row]["icon"].string!)!)
+        let icon = try! Data(contentsOf: URL(string: placeResultDisplay[indexPath.row]["icon"].string!)!)
         cell.imageView?.image = UIImage(data: icon, scale: CGFloat(1.5))
 
         return cell
@@ -73,7 +103,47 @@ class ResultsTableViewController: UITableViewController {
         return 80.0
     }
     
-
+    @IBAction func touchPrevious(_ sender: Any) {
+        nextButton.isEnabled = true
+        page -= 1
+        placeResultDisplay = placeResults[page]
+        if page == 0 {
+            previousButton.isEnabled = false
+        }
+        self.resultsTableView.reloadSections(IndexSet(arrayLiteral: 0), with: .right)
+    }
+    
+    @IBAction func touchNext(_ sender: Any) {
+        previousButton.isEnabled = true
+        if page < placeResults.count - 1 {
+            page += 1
+            placeResultDisplay = placeResults[page]
+            if page == placeResults.count - 1 {
+                checkNextButtonStatus()
+            }
+            self.resultsTableView.reloadSections(IndexSet(arrayLiteral: 0), with: .left)
+        }
+        else {
+            let url = URL(string: serverUrlPrefix + "moreplaces")
+            let parameters: Parameters = ["next_page_token": nextPageToken]
+            
+            Alamofire.request(url!, parameters: parameters).responseSwiftyJSON { (response) in
+                self.placeResultDisplay = response.result.value!["results"].arrayValue
+                self.placeResults.append(self.placeResultDisplay)
+                self.page += 1
+                if response.result.value!["next_page_token"].string != nil {
+                    self.nextPageToken = response.result.value!["next_page_token"].string!
+                }
+                else {
+                    self.nextPageToken = ""
+                }
+                self.checkNextButtonStatus()
+                self.resultsTableView.reloadSections(IndexSet(arrayLiteral: 0), with: .left)
+            }
+        }
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
